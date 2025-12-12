09a7aa5aa233086873972d82064dc571512586a0
Fink2005 <phans3806@gmail.com>
2025-12-13 01:42:52 +0700

    ci: trigger

--- app/shared/config/wagmiConfig.ts
+++ app/shared/config/wagmiConfig.ts
@@ -4,6 +4,8 @@ import { sepolia } from 'wagmi/chains'
 
 export const wagmiConfig = getDefaultConfig({
   appName: 'Fundhive DApp',
-  projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID!,
+  projectId:
+    import.meta.env.VITE_WALLETCONNECT_PROJECT_ID! ||
+    '0xF0FF7e67C2EBA417c7E0F0f8e1F8E7EA55c0aa73',
   chains: [sepolia]
 })
